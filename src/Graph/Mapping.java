b6d13f0babbb9d6ec3ee68a433e6c7e71fcfbcbf
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2024-09-03 14:38:33 +0530

    Mapping

---
+++ src/Graph/Mapping.java
@@ -0,0 +1,13 @@
+package Graph;
+import java.util.*;
+
+public class Mapping {
+    public static void main(String[] args) {
+        Map<Integer, String> mp = new HashMap();
+        mp.put(123, "English book");
+        mp.put(133, "Mathematics book");
+        mp.put(188, "DSA book for java");
+        System.out.println(mp.get(123));
+
+    }
+}
