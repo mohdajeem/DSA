0c5ba794b8a5ce209e7c86083ac2957866410b17
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2024-12-22 19:10:02 +0530

    using DP memoization

--- src/DynamicProgramming/ClimbingStairs.java
+++ src/DynamicProgramming/ClimbingStairs.java
@@ -8,6 +8,9 @@ public class ClimbingStairs {
         int n = sc.nextInt();
         int ans = countPaths(n);
         System.out.println("Ans : "+ans);
+        int ans2 = countsPathsDyn(n, new int[n+1]);
+        System.out.println("ans2: "+ans2);
+
 
     }
     public static int countPaths(int n){
@@ -16,7 +19,7 @@ public class ClimbingStairs {
         } else if(n<0){
             return 0;
         }
-        System.out.println("Hello "+n);
+        // System.out.println("Hello "+n);
         int nm1 = countPaths(n-1);
         int nm2 = countPaths(n-2);
         int nm3 = countPaths(n-3);
@@ -25,4 +28,21 @@ public class ClimbingStairs {
 
     }
 
+    public static int countsPathsDyn(int n, int[] qb){
+        if(n == 1){
+            return 1;
+        } else if(n<0){
+            return 0;
+        }
+
+        if(qb[n] > 0){
+            return qb[n];
+        }
+        int nm1 = countsPathsDyn(n-1, qb);
+        int nm2 = countsPathsDyn(n-2, qb);
+        int nm3 = countsPathsDyn(n-3, qb);
+        int cp = nm1+nm2+nm3;
+        qb[n] = cp;
+        return cp;
+    }
 }
